e80fa222a23001de7b0bd25d0b7cafa99f11a812
Nathan Randle <nrandle@dolbz.com>
2023-05-11 13:44:13 +0100

    Get day 20 part 2 working

--- 2022/day20/part2.py
+++ 2022/day20/part2.py
@@ -1,34 +1,40 @@
+decryption_key = 811589153
+
+
 def read_input_file():
-    file = open('input.txt', 'r')
+    file = open("input.txt", "r")
     lines = file.readlines()
 
-    return [(int(line) * 811589153, False) for line in lines]
+    encrypted_message = list()
+    for index, line in enumerate(lines):
+        encrypted_message.append((int(line) * decryption_key, index))
+
+    return encrypted_message
 
 
 encrypted_message = read_input_file()
 
-for _ in range(len(encrypted_message)):
-    current_index = 0
-    current_number = 0
+for _ in range(10):
+    for iteration_index in range(len(encrypted_message)):
+        for current_index, (current_number, original_index) in enumerate(
+            encrypted_message
+        ):
+            if original_index == iteration_index:
+                break
 
-    for index, (number, already_processed) in enumerate(encrypted_message):
-        if not already_processed:
-            current_index = index
-            current_number = number
-            break
-    new_index = current_index + current_number
+        new_index = current_index + current_number
 
-    if new_index >= len(encrypted_message):
-        new_index = new_index % (len(encrypted_message) - 1)
-    elif new_index < 0:
-        true_change = (abs(current_number) % (len(encrypted_message) - 1)) * -1
-        new_index = len(encrypted_message) - 1 + true_change
+        if new_index >= len(encrypted_message):
+            new_index = new_index % (len(encrypted_message) - 1)
+        elif new_index < 0:
+            true_change = (abs(new_index) % (len(encrypted_message) - 1)) * -1
+            new_index = len(encrypted_message) - 1 + true_change
 
-    encrypted_message.pop(current_index)
-    encrypted_message.insert(new_index, (number, True))
+        encrypted_message.pop(current_index)
+        encrypted_message.insert(new_index, (current_number, original_index))
 
-message_numbers_only = [number for (number, _) in encrypted_message]
-print(message_numbers_only)
+    message_numbers_only = [number for (number, _) in encrypted_message]
+    # print(message_numbers_only)
 
 while message_numbers_only[0] != 0:
     num = message_numbers_only.pop(0)
@@ -38,5 +44,5 @@ coord1 = message_numbers_only[1000 % len(message_numbers_only)]
 coord2 = message_numbers_only[2000 % len(message_numbers_only)]
 coord3 = message_numbers_only[3000 % len(message_numbers_only)]
 
-print(f'{coord1}, {coord2}, {coord3}')
+print(f"{coord1}, {coord2}, {coord3}")
 print(coord1 + coord2 + coord3)
